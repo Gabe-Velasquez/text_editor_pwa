222410f15aeb9349a2f04c31e75dff53729ad784
Gabe <gabriel.velasquez0116@gmail.com>
2023-11-03 16:02:44 -0700

    finished adding rules and creating plugins in webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,48 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Jate App',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'serviceWorker.js'
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Jate",
+        description: "Text Editor made with Javascript",
+        background_color:'#2196f3',
+        theme_color: '#2196f3',
+        start_url: './',
+        publicPath: './',
+        icons: [{
+          src: path.resolve('/src/images/logo.png'),
+          sizes: [480],
+          destination: path.join('assets','icons')
+        }]
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            }
+          }
+        }
       ],
     },
   };
